526bd52dc9dab8e80dab162cd14a3c539d9b3bd4
lgrante <louisgrante@outlook.fr>
2023-03-19 11:55:28 +0100

    configured dev script with nodemon + build script with tsc

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var secret_1 = require("./secret");
-(0, secret_1.default)()
-    .then(function () {
-    console.log(process.env.DATABASE_USERNAME);
-    console.log(process.env.DATABASE_PASSWORD);
-});
