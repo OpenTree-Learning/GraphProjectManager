c51daea3a2e9dde94698998bdc5e1e408e39d197
lgrante <louisgrante@outlook.fr>
2023-04-09 16:13:55 +0200

    added invitation mutation

--- backend/src/resolver/project.mutation.ts
+++ backend/src/resolver/project.mutation.ts
@@ -1,6 +1,7 @@
 import { OGM } from '@neo4j/graphql-ogm';
 
 import { Context  } from '../types';
+import { ProjectAuthJwt } from './types';
 import { verifyJwt } from '../utils/jwt_verify.utils';
 
 
@@ -49,11 +50,65 @@ async function createProject(
 	};
 }
 
+async function invite(
+	parent: any,
+	args: { email: string, as: string },
+	context: Context
+) {
+	const { ogm }: { ogm: OGM<any> } = context;
+	const invitation = ogm.model('Invitation');
+	const user = ogm.model('User');
+	let token: ProjectAuthJwt;
+
+	try {
+		token = await verifyJwt(context, process.env.JWT_SECRET as string) as ProjectAuthJwt;
+	} catch (err: any) {
+		return {
+			status: err.message,
+			data: null
+		};
+	}
+
+	if (!token.sub || !token.roles || !token.project_id) {
+		return {
+			status: 'Unexpected JWT payload.',
+			data: null
+		};
+	}
+
+	const [ users ] = await user.find({
+		where: {
+			email: args.email
+		}
+	});
+
+	if (!users) {
+		return {
+			status: 'User not found.',
+			data: null
+		};
+	}
+
+	const { invitations } = await invitation.create({
+		input: {
+			from: {connect: {where: {node: {id: token.sub}}}},
+			to: {connect: {where: {node: {email: args.email}}}},
+			project: {connect: {where: {node: {id: token.project_id}}}},
+			as: args.as
+		}
+	});
+
+	return {
+		status: `Invitation successfully sent to ${args.email}!`,
+		data: invitations[0]
+	};
+}
+
 const resolvers = {
 	Mutation: {
-		createProject
+		createProject,
+		invite
 	}
 };
-
 
 export default resolvers;
--- backend/src/resolver/types.ts
+++ backend/src/resolver/types.ts
@@ -1,5 +1,11 @@
+import {JwtPayload} from "jsonwebtoken";
+
 export interface MutationResponse {
 	status: string,
 	data: any
 }
 
+export interface ProjectAuthJwt extends JwtPayload {
+	roles: string [],
+	project_id: string
+}
